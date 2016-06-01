6e581c52a37ffb57128615c40663a7fb587b955d
kusabashira <kusabashira227@gmail.com>
2016-06-01 22:18:30 +0900

    Remove strange text in aoj/volume100/d

--- aoj/volume100/d/A10015.d
+++ aoj/volume100/d/A10015.d
@@ -37,38 +37,3 @@ void main() {
         }
     }
 }
-Compile Error Logs:
-
-Status
-Judge: 4/4 	D 	CPU: 00.00 sec 	Memory: 2484 KB 	Length: 705 B 	2016-05-28 21:48 	2016-05-28 21:48
-Results for testcases
-Case # 	Verdict 	CPU Time 	Memory 	In 	Out 	Case Name
-Case #1: 	: Accepted 	00.00 sec 	2460 KB 			
-Case #2: 	: Accepted 	00.00 sec 	2484 KB 			
-Case #3: 	: Accepted 	00.00 sec 	2452 KB 			
-Case #4: 	: Accepted 	00.00 sec 	2368 KB 			
-< prev | 	1 	/ 	4 	| next > 	  		 : Accepted        00.00 sec        2460 KB
- 
-Judge Input #1 ( in1.txt | 16 B) 		Judge Output #1 ( out1.txt | 16 B)
-
-In preparation.
-
-		
-
-In preparation.
-
-Comments
- 
- Under Construction.
- 
-Categories
- 
-Add Category: 	
-	
-	* Note for the classification 	
- 
-Free Tags
- 
-Add Tag: 		
-	Please add a tag related to 'algorithm'.
- 
